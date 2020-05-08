96bda5fb85233d22f0b506e45ce3cf479e6e40c4
Chernyshov_Nikita <xpom5566@mail.ru>
2020-05-08 22:07:09 +0300

    solved: 152ms (26.3%) / 5.26%, not the best but at least solved, wierd problem with lists allocation ... grid[:]

--- main.py
+++ main.py
@@ -4,37 +4,74 @@ import sys
 
 class Solution:
 
-  def bfs(self, i: int, j: int, pathObj: Dict[str, bool]) -> int:
-    if i == len(self.grid) - 1 and j == len(self.grid[0]) - 1:
-      # print('END!')
-      # pathSum = sum(pathObj.values()) + self.grid[i][j]
-      # print('path sum ', pathSum, pathObj)
-      return sum(pathObj.values())
+  def bfs(self, stack, minDistMap, checkMap) -> int:
+    print('--------------------------------')
+    print('stack', stack)
+    print('minDistMap', minDistMap)
+    print('checkMap', checkMap)
+    if len(stack) == 0:
+      print('end =)')
+      return 0
 
-    bottomPathSum = sys.maxsize
-    rightPathSum = sys.maxsize
+    # get first element coordinates from stack
+    i, j = stack.pop(0)
+    print('%d / %d' % (i, j))
 
-    if i + 1 < len(self.grid):
-      bottomPathObj = pathObj.copy()
-      bottomPathObj[str(i + 1) + ',' + str(j)] = self.grid[i + 1][j]
-      bottomPathSum = self.bfs(i + 1, j, bottomPathObj)
+    bottomPosition = sys.maxsize
+    rightPosition = sys.maxsize
+    if i + 1 < len(minDistMap):
+      bottomPosition = minDistMap[i + 1][j]
+    if j + 1 < len(minDistMap[0]):
+      rightPosition = minDistMap[i][j + 1]
 
-    if j + 1 < len(self.grid[0]):
-      rightPathObj = pathObj.copy()
-      rightPathObj[str(i) + ',' + str(j + 1)] = self.grid[i][j + 1]
-      rightPathSum = self.bfs(i, j + 1, rightPathObj)
+    currentMin = min(bottomPosition, rightPosition)
+    if currentMin == sys.maxsize:
+      currentMin = 0
 
-    return bottomPathSum if rightPathSum > bottomPathSum else rightPathSum
+    minDistMap[i][j] += currentMin
+
+    if i - 1 >= 0 and checkMap[i - 1][j] != True:
+      checkMap[i - 1][j] = True
+      stack.append([i - 1, j])
+      print('added i -1 ', stack)
+
+    if j - 1 >= 0 and checkMap[i][j - 1] != True:
+      checkMap[i][j - 1] = True
+      stack.append([i, j - 1])
+      print('added j -1', stack)
+
+    self.bfs(stack, minDistMap, checkMap)
 
   def minPathSum(self, grid: List[List[int]]) -> int:
-    self.grid = grid
+    '''
+    # Problem with coping grid list to new checkMap ...
+    checkMap = grid[:]
+
+    for i in range(len(checkMap)):
+      for j in range(len(checkMap[0])):
+        checkMap[i][j] = False
+    '''
+
+    checkMap = []
+    for i in range(len(grid)):
+      checkMap.append([])
+      for j in range(len(grid[0])):
+        checkMap[i].append(False)
+
+    print('checkMap', checkMap)
+
+    minDistMap = grid[:]
+    stack = [[len(minDistMap) - 1, len(minDistMap[0]) - 1]]
+    self.bfs(stack, minDistMap, checkMap)
+    # print(minDistMap)
+
     # TODO: find path from the end to start, get min value and that all !!!
-    return self.bfs(0, 0, {'0,0': grid[0][0]})
+    return minDistMap[0][0]
 
 
 my = Solution()
 n = [[1, 3, 1], [1, 5, 1], [4, 2, 1]]
-# TODO: find path from the end to start, get min value and that all !!!
+# TODO: find path from the end to start, get min value and that's all !!!
 # TOO SLOW !!! =)))))
 # n = [[7, 1, 3, 5, 8, 9, 9, 2, 1, 9, 0, 8, 3, 1, 6, 6, 9, 5],
 #      [9, 5, 9, 4, 0, 4, 8, 8, 9, 5, 7, 3, 6, 6, 6, 9, 1, 6],
@@ -50,3 +87,20 @@ n = [[1, 3, 1], [1, 5, 1], [4, 2, 1]]
 #      [0, 1, 6, 1, 1, 0, 0, 6, 5, 4, 3, 4, 3, 7, 9, 6, 1, 9]]
 ans = my.minPathSum(n)
 print("ans", ans)
+'''
+# from end to start, on each step get sum = min(prevBottom, prevRight) + currentStep
+# for Each step
+n = [
+  [1, 3, 1],
+  [1, 5, 1],
+  [4, 2, 1]
+]
+n = [
+  [7, 6, 3],
+  [8, 7, 2],
+  [7, 3, 1]
+]
+'''
+
+# Runtime: 152 ms, faster than 26.33% of Python3 online submissions for Minimum Path Sum.
+# Memory Usage: 49.2 MB, less than 5.26% of Python3 online submissions for Minimum Path Sum.
